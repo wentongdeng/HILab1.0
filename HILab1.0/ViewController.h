1683005461601c3368008fd88c446298d1329cc9
dengwt <dengwt@dengwtdeMacBook-Pro.local>
2017-02-10 20:59:40 +0800

    Initial Commit

---
+++ HILab1.0/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HILab1.0
+//
+//  Created by dengwt on 2017/2/10.
+//  Copyright © 2017年 dengwt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HILab1.0/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HILab1.0
+//
+//  Created by dengwt on 2017/2/10.
+//  Copyright © 2017年 dengwt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
